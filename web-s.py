d095040fa9b7465a68d2b3fd5be51a9e900c09f4
Hiren Vadher <hirenvadher954@gmail.com>
2020-02-09 23:24:49 +0530

    first commit

--- web-s.py
+++ web-s.py
@@ -2,9 +2,9 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-products = []  # List to store name of the product
-prices = []  # List to store price of the product
-ratings = []  # List to store rating of the product
+products = []
+prices = []
+
 
 driver = webdriver.Chrome(executable_path="./chromedriver")
 driver.get(
@@ -18,7 +18,7 @@ for a in soup.findAll('a', href=True, attrs={'class': '_31qSD5'}):
     rating = a.find('div', attrs={'class': 'hGSR34 _2beYZw'})
     products.append(name.text)
     prices.append(price.text)
-    # ratings.append(rating.text)
+
 
 df = pd.DataFrame({'Product Name': products, 'Price': prices})
 df.to_csv('products.csv', index=False, encoding='utf-8')
